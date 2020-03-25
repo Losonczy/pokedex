032100dc2083cf7deab00254cac68a7ca4276b26
Bence Losonczy <losonzybence@gmail.com>
2020-03-25 13:35:52 +0100

    creating components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-undef */
 import React from "react";
 import "./App.css";
+import NavBar from "./Components/layout/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Asd</h1>
+      <NavBar />
     </div>
   );
 }
