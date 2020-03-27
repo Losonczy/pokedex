60f9a553190ab3aa101d18e59275c9b69190581e
Bence Losonczy <losonzybence@gmail.com>
2020-03-27 10:05:45 +0100

    starting to implement pokemon details, fixed some major issues

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import "./App.css";
 import NavBar from "./Components/layout/NavBar";
 import DashBoard from "./Components/layout/DashBoard";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Pokemon from "./Components/pokemon/Pokemon";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div className="container">
-        <DashBoard />
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={DashBoard} />
+            <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Components/pokemon/PokemonCard.js
+++ src/Components/pokemon/PokemonCard.js
@@ -1,43 +1,54 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Sprite = styled.img`
-  width: 5em;
-  height: 5em;
+  width: 6em;
+  height: 6em;
+`;
+
+const Card = styled.div`
+  box-shadow: 0 1px 3 px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
 `;
 
 export default class PokemonCard extends Component {
   state = {
     name: "",
     imageUrl: "",
-    pokemondIndex: ""
+    pokemonIndex: ""
   };
 
   componentDidMount() {
     const name = this.props.name;
     const url = this.props.url;
-    const pokemondIndex = url.split("/")[url.split("/").length - 2];
-    const imageUrl = `https://github.com/PokeApi/sprites/blob/master/sprites/pokemon/${pokemondIndex}.png?raw=true`;
+    const pokemonIndex = url.split("/")[url.split("/").length - 2];
+    const imageUrl = `https://github.com/PokeApi/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
     this.setState({
       name,
       imageUrl,
-      pokemondIndex
+      pokemonIndex
     });
   }
 
   render() {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
-        <div className="card">
-          <h5 className="card-header">{this.state.pokemondIndex}</h5>
-          <Sprite
-            className="card-img-top rounded mx-auto mt-2"
-            src={this.state.imageUrl}
-          ></Sprite>
-          <div className="card-body mx-auto">
-            <h6 className="card-title">{this.state.name}</h6>
-          </div>
-        </div>
+        <Link to={`pokemon/${this.state.pokemonIndex}`}>
+          <Card className="card">
+            <h5 className="card-header">{this.state.pokemonIndex}</h5>
+            <Sprite
+              className="card-img-top rounded mx-auto mt-2"
+              src={this.state.imageUrl}
+            ></Sprite>
+            <div className="card-body mx-auto">
+              <h6 className="card-title">{this.state.name}</h6>
+            </div>
+          </Card>
+        </Link>
       </div>
     );
   }
